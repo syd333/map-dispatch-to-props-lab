d932f87f3e410cee52f05508d536225fc1c7e30c
syd333 <sydmiranda@icloud.com>
2021-04-07 16:03:48 -0700

    commit tree 35fb1a85c3e64606059f39e118bce658da8e8f58

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -22,8 +22,8 @@ export class RestaurantInput extends Component {
   }
 
   handleOnSubmit = event => {
-    event.preventDefault();
-    // add missing code
+    event.preventDefault()
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +49,17 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = state => {
+  return state
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (restaurant) => {
+      dispatch(addRestaurant(restaurant))
+    }
+  }
+}
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(mapStateToProps,mapDispatchToProps)(RestaurantInput)
